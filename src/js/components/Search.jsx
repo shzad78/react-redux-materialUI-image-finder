1fa9f55b5e67fd281e793cb4eae27ebcb5858ffa
shahzad babar <shzad78@gmail.com>
2018-05-07 23:02:23 +0500

    ïmages

--- src/js/components/Search.jsx
+++ src/js/components/Search.jsx
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
-import TextField from 'material-ui/TextField';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
+import React, { Component } from "react";
+import TextField from "material-ui/TextField";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
 
- class Search extends Component {
-     
-        state= {
-         searchText: '',
-         amount: 15,
-         apiUrl: 'https://pixabay.com/api',
-         apiKey: '8919227-c987f39bcc16f253cc149b502',
-         images: []
-     }
-     onTextChange=(e)=> {
-         this.setState({searchText: })
-
-     }
+class Search extends Component {
+  state = {
+    searchText: "",
+    amount: 15,
+    apiUrl: "https://pixabay.com/api",
+    apiKey: "8919227-c987f39bcc16f253cc149b502",
+    images: []
+  };
+  onTextChange = e => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      axios
+        .get(
+          `${this.state.apiUrl}/?key=${this.state.apiKey}&
+      q=${this.state.searchText}&per_page=${this.state.amount}`
+        )
+        .then(res => this.setState({ images: res.data.hits }))
+        .catch(err => console.log(err));
+    });
+  };
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <TextField
-        name= "searchText"
-        value= {this.state.searchText}
-        onChange= {this.onTextChange}
-        floatingLabelText= "Search for Images"
-        fullWidth= {true}
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search for Images"
+          fullWidth={true}
         />
         <br />
         <SelectField
@@ -38,9 +46,8 @@ import MenuItem from 'material-ui/MenuItem';
           <MenuItem value={30} primaryText="30" />
           <MenuItem value={50} primaryText="50" />
         </SelectField>
-        
       </div>
-    )
+    );
   }
 }
 export default Search;
