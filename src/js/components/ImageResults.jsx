4b9c965ba663209365cdb8146bc263e27bfb3feb
shahzad babar <shzad78@gmail.com>
2018-05-08 00:03:46 +0500

    ïmages

---
+++ src/js/components/ImageResults.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { GridList, GridTile } from "material-ui/GridList";
+
+class ImageResults extends Component {
+  render() {
+    let imageListContent;
+    const { images } = this.props;
+    if (images) {
+      imageListContent = (
+        <GridList cols={3}>
+          {images.map(img => (
+            <GridTile title={img.tags}>
+              <img src={img.largeImageURL} alt="" />
+            </GridTile>
+          ))}
+        </GridList>
+      );
+    } else {
+      imageListContent = null;
+    }
+    return <div>{imageListContent}</div>;
+  }
+}
+export default ImageResults;
