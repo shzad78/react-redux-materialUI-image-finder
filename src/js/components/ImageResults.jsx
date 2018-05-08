cbebdab584cf448cccb9e878fd86cb95f824d0b2
shahzad babar <shzad78@gmail.com>
2018-05-08 23:16:30 +0500

    done
    git commit -m done

--- src/js/components/ImageResults.jsx
+++ src/js/components/ImageResults.jsx
@@ -4,8 +4,10 @@ import { GridList, GridTile } from "material-ui/GridList";
 class ImageResults extends Component {
   render() {
     let imageListContent;
+    const { text } = this.props;
     const { images } = this.props;
-    if (images) {
+    console.log("search text is ", text);
+    if (text) {
       imageListContent = (
         <GridList cols={3}>
           {images.map(img => (
--- src/js/components/Search.jsx
+++ src/js/components/Search.jsx
@@ -4,24 +4,28 @@ import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
 import axios from "axios";
 import ImageResults from "./ImageResults";
-import * as actions from '../actions';
-import {connect} from 'react-redux';
+import * as actions from "../actions";
+import { connect } from "react-redux";
 
 class Search extends Component {
   state = {
     searchText: "",
     num: ""
   };
-  
-  onAmountChange = (e, index, value) => {this.setState({ num: value });};
+
+  onAmountChange = (e, index, value) => {
+    this.setState({ num: value });
+  };
   render() {
-    console.log('this is data from search',this.props.data);
+    console.log("this is data from search", this.props.data);
     return (
       <div>
         <TextField
           name="searchText"
           value={this.state.searchText}
-          onChange={(e)=>{this.setState({searchText:e.target.value})}}
+          onChange={e => {
+            this.setState({ searchText: e.target.value });
+          }}
           floatingLabelText="Search for Images"
           fullWidth={true}
         />
@@ -31,9 +35,7 @@ class Search extends Component {
           floatingLabelText="Amount"
           onChange={this.onAmountChange}
           value={this.state.num}
-          
         >
-        
           <MenuItem value={5} primaryText="5" />
           <MenuItem value={10} primaryText="10" />
           <MenuItem value={15} primaryText="15" />
@@ -43,18 +45,31 @@ class Search extends Component {
         <br />
         <br />
         <br />
-        <button onClick={()=>{this.props.getPics(this.state)}}>Search</button>
+
+        <button
+          onClick={() => {
+            this.props.getPics(this.state);
+          }}
+        >
+          Search
+        </button>
+
         <br />
         {Object.keys(this.props.data).length ? (
-          <ImageResults images={this.props.data.hits} />
-        ) : []}
+          <ImageResults
+            images={this.props.data.hits}
+            text={this.state.searchText}
+          />
+        ) : (
+          []
+        )}
       </div>
     );
   }
 }
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    data:state.data.data
-  }
+    data: state.data.data
+  };
 }
-export default connect(mapStateToProps,{getPics:actions.fetchData})(Search);
+export default connect(mapStateToProps, { getPics: actions.fetchData })(Search);
